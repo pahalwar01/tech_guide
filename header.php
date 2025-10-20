05aefec3411cc883953e8c88c35d689782e1785d
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2025-10-21 01:28:47 +0530

    Update header.php

--- header.php
+++ header.php
@@ -8,11 +8,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="css/style.css">
-  <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+  <link rel="icon" type="image/x-icon" href="img/logo_trans.png">
 </head>
 <body>
   <header>
-    <h1 class="brand-name" style="color: rgb(250, 255, 0);"><b><?php include 'brand_name.php'; ?></b></h1>
+    <h1 class="brand-name"><b><?php include 'brand_name.php'; ?></b></h1>
     <p class="brand-slogan"><b>"Your Journey, Our Responsibility"</b></p>
     <p class="brand-slogan-lastline" style="margin-top: -10px;">"Trusted tours and travels company in Delhi & NCR."</p>
   </header>
