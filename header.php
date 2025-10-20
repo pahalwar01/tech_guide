e8b768875b9bf367cb0ae51e0cceeaec8c6080d8
pahalwar01 <98372242+pahalwar01@users.noreply.github.com>
2025-10-21 00:08:01 +0530

    Update header.php

--- header.php
+++ header.php
@@ -11,25 +11,7 @@
   <link rel="icon" type="image/x-icon" href="img/favicon.ico">
 </head>
 <body>
-
-  <!--header name and contact details-->
-    <div class="topbar">
-        <div><strong><?php include 'brand_name.php'; ?></strong></div>
-            <a>
-                📞 +91 9876543210 | ✉ travelagency@email.com
-            </a>
-    </div>
-  <!--header name and contact details end-->
-
-  <header style="background:url('https://cdn.pixabay.com/photo/2020/03/01/00/55/compass-4891499_1280.jpg')
-                center/cover no-repeat;  
-                height:20vh; 
-                color:white; 
-                text-align:center; 
-                display:flex; 
-                flex-direction:column; 
-                justify-content:center; 
-                position:relative;">
+  <header>
     <h1 class="brand-name" style="color: rgb(250, 255, 0);"><b><?php include 'brand_name.php'; ?></b></h1>
     <p class="brand-slogan"><b>"Your Journey, Our Responsibility"</b></p>
     <p class="brand-slogan-lastline" style="margin-top: -10px;">"Trusted tours and travels company in Delhi & NCR."</p>
